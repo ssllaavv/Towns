61a9c3b7a7ef78d1ee7b2ebcad0ed52712addb76
ssllaavv-dev <ssllaavv.devops@students.softuni.bg>
2024-03-03 12:35:32 +0200

    Implemented functionality to add a new town

--- towns.js
+++ towns.js
@@ -2,18 +2,42 @@ $(document).ready(function() {
 	$('#btnDelete').click(deleteTown)
 });
 
+// function deleteTown() {
+// 	let townName = $('#townName').val();
+// 	$('#townName').val('');
+// 	let removed = false;
+// 	for (let option of $('#towns option')) {
+// 		if (option.textContent == townName) {
+// 			removed = true;
+// 			option.remove();
+// 		}
+// 	}
+// 	if (removed)
+// 		$('#result').text(townName + " deleted.");
+// 	else
+// 		$('#result').text(townName + " not found.");
+// }
+
+
 function deleteTown() {
-	let townName = $('#townName').val();
-	$('#townName').val('');
-	let removed = false;
-	for (let option of $('#towns option')) {
-		if (option.textContent == townName) {
-			removed = true;
-			option.remove();
-		}
-	}
-	if (removed)
-		$('#result').text(townName + " deleted.");
-	else
-		$('#result').text(townName + " not found.");
+    let townName = $('#townName').val();
+    $('#townName').val('');
+    let removed = false;
+    for (let option of $('#towns option')) {
+        if (option.textContent == townName) {
+            removed = true;
+            option.remove();
+        }
+    }
+    if (removed)
+        showMessage(townName + " deleted.");
+    else
+        showMessage(townName + " not found.");
+}
+
+function showMessage(msg) {
+    $('#result').text(msg).css("display", "block");
+    setTimeout(function () {
+        $('#result').hide('blind', {}, 500);
+    }, 3000);
 }
